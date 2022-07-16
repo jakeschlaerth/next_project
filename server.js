e98d4308d75399f3fcef2b97a78c8f63a316ad33
jakeschlaerth <jakeschlaerth1999@gmail.com>
2022-07-16 16:51:30 -0400

    WIP containerization

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const LoginController = require('./controllers/LoginController');
 const PlantController = require('./controllers/PlantController');
 
 var port = 3000;
-
+const host = "0.0.0.0";
 // User
 app.post('/auth', LoginController.authenticateUser);
 app.post('/register', LoginController.registerUser);
@@ -16,6 +16,6 @@ app.post('/plants', PlantController.addPlant);
 app.put('/plants', PlantController.updatePlant);
 app.delete('/plants', PlantController.deletePlant);
 
-var server = app.listen(port, function () {
+var server = app.listen(port, host, () => {
 	console.log('Express server listening on port ' + port);
 });
